76e81d504666a3d7503dc544410aba48468afe5e
Fateme <fa.bagherzad@gmail.com>
2024-10-03 09:50:08 -0400

    get top camp base on rating

--- client-camp-heaven/src/components/TopCampOfYear/TopCampOfYear.jsx
+++ client-camp-heaven/src/components/TopCampOfYear/TopCampOfYear.jsx
@@ -1,16 +1,28 @@
 import './TopCampOfYear.scss';
 
 const TopCampOfYear = ({ allCamps }) => {
+  const topCamp = allCamps.sort(
+    (a, b) => b.ratingsAverage - a.ratingsAverage
+  )[0];
+
   return (
     <div className="dashboardTopCamp">
-      <h2>Top Camp Of Year</h2>
-      <img
-        src={allCamps[0].imageCover}
-        alt=""
-        className="dashboardTopCamp__img"
-      />
-      <p>{allCamps[0].description}</p>
+      <h2>Top Camp Of The Year</h2>
+      {topCamp ? ( // Check if topCamp exists to avoid errors
+        <>
+          <img
+            src={topCamp.imageCover}
+            alt={topCamp.name} // Provide a meaningful alt text
+            className="dashboardTopCamp__img"
+          />
+          <h3>{topCamp.name}</h3>
+          <p>{topCamp.description}</p>
+        </>
+      ) : (
+        <p>No camps available</p> // Fallback if no camp data is present
+      )}
     </div>
   );
 };
+
 export default TopCampOfYear;
